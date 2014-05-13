57644ee7e49d238cb499fa08f2f88e35758b84b2
Mok-SK <iruttkayn@gmail.com>
2014-05-13 15:56:25 +0200

    fix - oprava 'eventTypeUri' v triede 'MonitoringStartedEventDto'.

--- useractivity.app/src/main/java/com/gratex/perconik/useractivity/app/dto/MonitoringStartedEventDto.java
+++ useractivity.app/src/main/java/com/gratex/perconik/useractivity/app/dto/MonitoringStartedEventDto.java
@@ -1,11 +1,40 @@
 package com.gratex.perconik.useractivity.app.dto;
 
 import java.util.UUID;
+
 import javax.ws.rs.core.UriBuilder;
+
 import com.gratex.perconik.useractivity.app.Settings;
 
-public class MonitoringStartedEventDto extends ApplicationEventDto {
+public class MonitoringStartedEventDto extends SystemEventDto {
 	private static final String SESSION_ID = UUID.randomUUID().toString();
+	private String appName;
+	private String appVersion;
+	private String sessionId;
+	
+	public String getAppName() {
+		return appName;
+	}
+	
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	
+	public String getAppVersion() {
+		return appVersion;
+	}
+	
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+	
+	public String getSessionId() {
+		return sessionId;
+	}
+	
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
 	
 	public MonitoringStartedEventDto() {
 		setAppName("UACA");
