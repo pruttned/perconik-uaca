e57a693c018d937123aac1a946cb14228b5a484c
Mok-SK <iruttkayn@gmail.com>
2014-05-16 09:11:09 +0200

    Úprava nasadenia - User Activity centrálna aplikácia sa spúšťa pre aktuálneho používateľa.

--- useractivity.app/src/deb/data/run.sh
+++ useractivity.app/src/deb/data/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#init
+if [ "$1" == "-init" ]
+then
+	echo "Running the initialization..."
+	cp /opt/gti/perconik/uaca/perconik-uaca.desktop ~/.config/autostart/perconik-uaca.desktop
+	echo "Initialization finished"
+fi
+
 #Executing 'uaca.jar'
 java -Djava.library.path="/opt/gti/perconik/uaca/libs" -jar "/opt/gti/perconik/uaca/[JarName]" &
